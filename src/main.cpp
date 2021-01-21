a2fbf19d44966470edced95ae0701d573fb8cbbc
Bill Smithem <bill@charlestonpr.com>
2021-01-21 18:05:02 -0500

    Modified to use rotary encoder button press as "shift" key - sends two different codes from encoder when button pushed simultaneously with rotation. Went from 33 to 41 "buttons"

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,12 @@
 // 4/4/2020
 // Bill Smithem
 // set debounce time to 50 (default was 10)
+// 01/21/2021
+// Bill Smtihem
+// add "4 func" rotaries - different code sent if button pressed while turning. Tracked state of buttons 21-24
+//  and used that to "shift" the rotary encoders. Note: we're still sending the button presses for the rotary
+//  encoders, so mind that when assigning buttons in game.
+// Number of "buttons" goes from 33 to 41
 
 #include <Arduino.h>
 #include <Keypad.h>
@@ -22,15 +28,8 @@
 #define NUMROWS 5
 #define NUMCOLS 5
 
-
-// byte buttons[NUMROWS][NUMCOLS] = {
-//   {0, 1, 2, 3, 4},
-//   {5, 6, 7, 8, 9},
-//   {10, 11, 12, 13, 14},
-//   {15, 16, 17, 18, 19},
-//   {20, 21, 22, 23, 24},
-// };
-
+// Odd map is so buttons come out in numberical order, not scanning order. i.e. top left button is one, next is
+//  two, etc.
 byte buttons[NUMROWS][NUMCOLS] = {
   {5, 6, 17, 23, 12},
   {3, 4, 16, 22, 11},
@@ -42,16 +41,18 @@ byte buttons[NUMROWS][NUMCOLS] = {
 struct rotariesdef {
   byte pin1;
   byte pin2;
-  int ccwchar;
-  int cwchar;
+  int ccwchar;      // send this for CCW
+  int cwchar;       //  this for CW
+  int ccwPchar;     //  this for CCW with button pressed
+  int cwPchar;      //  and this for CW with button pressed
   volatile unsigned char state;
 };
 
 rotariesdef rotaries[NUMROTARIES] {
-  {0, 1, 32, 31, 0},
-  {2, 3, 29, 30, 0},
-  {4, 5, 27, 28, 0},
-  {6, 7, 25, 26, 0},
+  {0, 1, 32, 31, 40, 39, 0},
+  {2, 3, 29, 30, 38, 37, 0},
+  {4, 5, 27, 28, 36, 35, 0},
+  {6, 7, 25, 26, 34, 33, 0},
 };
 
 #define DIR_CCW 0x10
@@ -110,7 +111,7 @@ byte colPins[NUMCOLS] = {14, 16, 10, 9, 8};
 Keypad buttbx = Keypad( makeKeymap(buttons), rowPins, colPins, NUMROWS, NUMCOLS);
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
-                   JOYSTICK_TYPE_JOYSTICK, 33, 0,
+                   JOYSTICK_TYPE_JOYSTICK, 41, 0,
                    false, false, false, false, false, false,
                    false, false, false, false, false);
 
@@ -132,21 +133,31 @@ void loop() {
 
 }
 
+// keep state of last four buttons (ones attached to rotaries)
+KeyState rbState[] = { RELEASED, RELEASED, RELEASED, RELEASED };
+
 void CheckAllButtons(void) {
+  int kc;
+
   if (buttbx.getKeys())
   {
     for (int i = 0; i < LIST_MAX; i++)
     {
       if ( buttbx.key[i].stateChanged )
       {
+        kc = buttbx.key[i].kchar - 21;        // track state of buttons 21-24 (switches on rotaries)
         switch (buttbx.key[i].kstate) {
         case PRESSED:
         case HOLD:
           Joystick.setButton(buttbx.key[i].kchar, 1);
+          if (kc >= 0)
+            rbState[kc] = PRESSED;
           break;
         case RELEASED:
         case IDLE:
           Joystick.setButton(buttbx.key[i].kchar, 0);
+          if (kc >= 0)
+            rbState[kc] = RELEASED;
           break;
         }
       }
@@ -177,11 +188,20 @@ void CheckAllEncoders(void) {
   for (int i = 0; i < NUMROTARIES; i++) {
     unsigned char result = rotary_process(i);
     if (result == DIR_CCW) {
-      Joystick.setButton(rotaries[i].ccwchar, 1); delay(50); Joystick.setButton(rotaries[i].ccwchar, 0);
-    };
+      // check if button pressed
+      if (rbState[3-i] == PRESSED) {
+        Joystick.setButton(rotaries[i].ccwPchar, 1); delay(50); Joystick.setButton(rotaries[i].ccwPchar, 0);
+      } else {
+        Joystick.setButton(rotaries[i].ccwchar, 1); delay(50); Joystick.setButton(rotaries[i].ccwchar, 0);
+      }
+    }
     if (result == DIR_CW) {
-      Joystick.setButton(rotaries[i].cwchar, 1); delay(50); Joystick.setButton(rotaries[i].cwchar, 0);
-    };
+      if (rbState[3-i] == PRESSED) {
+        Joystick.setButton(rotaries[i].cwPchar, 1); delay(50); Joystick.setButton(rotaries[i].cwPchar, 0);
+      } else {
+        Joystick.setButton(rotaries[i].cwchar, 1); delay(50); Joystick.setButton(rotaries[i].cwchar, 0);
+      }
+    }
   }
 }
 
